fbc90ef72269d7ec1ea2fe3383e2683273bb97d7
Zach Kelling <z@zeekay.io>
2026-02-04 15:46:45 -0800

    chore: sync dependencies and format code

--- protocols/quasar/quasar.go
+++ protocols/quasar/quasar.go
@@ -34,8 +34,8 @@ import (
 
 	"github.com/luxfi/crypto/bls"
 	"github.com/luxfi/corona/threshold"
-	blsThreshold "github.com/luxfi/threshold/protocols/bls"
 	"github.com/luxfi/threshold/pkg/party"
+	blsThreshold "github.com/luxfi/threshold/protocols/bls"
 )
 
 var (
